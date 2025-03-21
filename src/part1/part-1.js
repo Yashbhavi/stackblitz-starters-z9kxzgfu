111a986d64d0c1a2f6d24d50198be67db010807d
Yashbhavi <git@stackblitz.com>
2025-03-21 23:19:51 +0530

    Initial commit

---
+++ src/part1/part-1.js
@@ -0,0 +1,56 @@
+/**
+ * Refer to the following data when completing your answers: (The following JSON
+ * file contains providers available for an industry)
+ */
+
+const PROVIDERS = require('../shared/data.json');
+
+/**
+ * Create a query that returns total premiums and fees available for retail industry
+ */
+function task1() {
+  /**
+   * TODO: Your body goes here
+   */
+}
+
+/**
+ * Create a query that returns the minimum premium available among the providers for technology industry
+ */
+function task2() {
+  /**
+   * TODO: Your body goes here
+   */
+}
+
+/**
+ * Create a query that returns all the provider names available for the technology industry
+ * that has a premium amount greater than or equal to 1000
+ */
+function task3() {
+  /**
+   * TODO: Your body goes here
+   */
+}
+
+/**
+ * Create a query that returns  provider's name and industry who has the highest premium price
+ */
+
+function task4() {
+  /**
+   * TODO: Your body goes here
+   */
+}
+
+/**
+ * To see the expected results, please refer to ./expected-data.json file in part1 directory!
+ * NOTE: Please ensure that all the tests in part-1.spec.js are passing!
+ */
+
+module.exports = {
+  task1,
+  task2,
+  task3,
+  task4,
+};
